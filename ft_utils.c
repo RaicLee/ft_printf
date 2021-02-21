bbb7303e2827a7113e5e15beeee19637fecafe83
RaicLee <raiclee@gmail.com>
2021-02-21 20:57:58 +0900

    char

--- ft_utils.c
+++ ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: jealee <jealee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 16:18:52 by jealee            #+#    #+#             */
-/*   Updated: 2021/02/19 20:47:05 by jealee           ###   ########.fr       */
+/*   Updated: 2021/02/21 20:57:34 by jealee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	block_initialize(t_info *block)
 {
-	block->minus = 0;
-	block->zero = 0;
-	block->width = 0;
-	block->precision = -1;
-	block->type = 0;
-	block->base_num = 10;
-	block->sign_num = 1;
+	block->m = 0;
+	block->z = 0;
+	block->w = 0;
+	block->p = -1;
+	block->t = 0;
+	block->b = 10;
+	block->s = 1;
 }
 
 int		ft_putchar(char ch)
